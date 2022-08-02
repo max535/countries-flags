c9f1d4952ccfcd3cb31f115d518644c89e7fd02a
a.arutyunyan <a.arutyunyan@netrika.ru>
2022-08-02 12:38:00 +0300

    Настройка роутинга

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,33 @@
-import axios from 'axios';
-import { useEffect } from 'react';
 import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
 
-import { Card } from './components/Card';
-import { Controls } from './components/Controls';
 import { Header } from './components/Header';
-import { List } from './components/List';
 import { Main } from './components/Main';
-import { ALL_COUNTRIES } from './config';
+import { NotFound } from './pages/NotFound';
+import { Details } from './pages/Details';
+import { Homepage } from './pages/Homepage';
 
 export function App() {
     const [countries, setCountries] = useState([]);
-
-    console.log(countries);
-    useEffect(() => {
-        axios.get(ALL_COUNTRIES).then(({ data }) => setCountries(data));
-    }, []);
 
     return (
         <>
             <Header />
             <Main>
-                <Controls />
-                <List>
-                    {countries.map((c) => {
-                        const countryInfo = {
-                            img: c.flags.png,
-                            name: c.name,
-                            info: [
-                                {
-                                    title: 'Population',
-                                    description: c.population.toLocaleString(),
-                                },
-                                {
-                                    title: 'Region',
-                                    description: c.region,
-                                },
-                                {
-                                    title: 'Capital',
-                                    description: c.capital,
-                                },
-                            ],
-                        };
-
-                        return <Card key={c.name} {...countryInfo} />;
-                    })}
-                </List>
+                <Routes>
+                    <Route
+                        exact
+                        path='/'
+                        element={
+                            <Homepage
+                                countries={countries}
+                                setCountries={setCountries}
+                            />
+                        }
+                    />
+                    <Route path='/country/:name' element={<Details />} />
+                    <Route path='*' element={<NotFound />} />
+                </Routes>
             </Main>
         </>
     );
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,54 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { List } from '../components/List';
+import { Card } from '../components/Card';
+import { Controls } from '../components/Controls';
+import { ALL_COUNTRIES } from '../config';
+
+export const Homepage = ({ countries, setCountries }) => {
+    let navigate = useNavigate();
+
+    useEffect(() => {
+        if (!countries.length) {
+            axios.get(ALL_COUNTRIES).then(({ data }) => setCountries(data));
+        }
+    }, []);
+
+    return (
+        <>
+            <Controls />
+            <List>
+                {countries.map((c) => {
+                    const countryInfo = {
+                        img: c.flags.png,
+                        name: c.name,
+                        info: [
+                            {
+                                title: 'Population',
+                                description: c.population.toLocaleString(),
+                            },
+                            {
+                                title: 'Region',
+                                description: c.region,
+                            },
+                            {
+                                title: 'Capital',
+                                description: c.capital,
+                            },
+                        ],
+                    };
+
+                    return (
+                        <Card
+                            key={c.name}
+                            onClick={() => navigate(`/country/${c.name}`)}
+                            {...countryInfo}
+                        />
+                    );
+                })}
+            </List>
+        </>
+    );
+};

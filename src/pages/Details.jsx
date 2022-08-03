a86ba4d0be9552fd56b4080aed56f4311920cc06
a.arutyunyan <a.arutyunyan@netrika.ru>
2022-08-03 07:13:19 +0300

    Добавляет детальную страницу

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,7 +1,33 @@
-import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { IoArrowBack } from 'react-icons/io5';
+
+import { searchByCountry } from '../config';
+import { Button } from '../components/Button';
+import { Info } from '../components/Info';
 
 export const Details = () => {
     const { name } = useParams();
+    const navigate = useNavigate();
+    const push = useNavigate();
 
-    return <div>Details {name}</div>;
+    const [country, setCountry] = useState(null);
+
+    console.log(country);
+
+    useEffect(() => {
+        axios
+            .get(searchByCountry(name))
+            .then(({ data }) => setCountry(data[0]));
+    }, [name]);
+
+    return (
+        <div>
+            <Button onClick={() => navigate(-1)}>
+                <IoArrowBack /> Back
+            </Button>
+            {country && <Info push={push} {...country} />}
+        </div>
+    );
 };
